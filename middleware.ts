f6ed07d198a209d42772ca0ca2f3368f128c23a3
domniea <domniea@gmail.com>
2025-03-03 15:48:00 -0700

    test with old middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,6 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
 
-export const { auth: middleware } = NextAuth(authConfig);
+// export const { auth: middleware } = NextAuth(authConfig);
+
+export { auth as middleware } from '@/auth'
