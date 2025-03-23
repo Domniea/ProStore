7d987e1a8902708614d4c7a1abc5197beb1eba1c
domniea <domniea@gmail.com>
2025-03-23 10:06:51 -0600

    create order action

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -3,6 +3,8 @@ import { auth } from "@/auth";
 import { getUserById } from "@/lib/actions/user.actions";
 import PaymentMethodForm from "./payment-method-form";
 import CheckoutSteps from "@/components/shared/checkout-steps";
+import { getMyCart } from "@/lib/actions/cart.actions";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Select payment Method",
@@ -11,11 +13,13 @@ export const metadata: Metadata = {
 const PaymentMethod = async () => {
   const session = await auth();
   const userId = session?.user?.id;
+  const cart = await getMyCart()
 
   if (!userId) throw new Error("User not Found");
 
   const user = await getUserById(userId);
 
+  if (!cart || cart.items.length === 0) redirect('/cart')
   return (
     <>
       <CheckoutSteps current={2} />
--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -2,10 +2,10 @@ import { auth } from "@/auth";
 import { getMyCart } from "@/lib/actions/cart.actions";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
-// import { ShippingAddress } from "@/types";
+import { ShippingAddress } from "@/types";
 import { getUserById } from "@/lib/actions/user.actions";
 import ShippingAddressForm from "./shipping-address-form";
-import { ShippingAddress } from "@/types";
+
 import CheckoutSteps from "@/components/shared/checkout-steps";
 
 
