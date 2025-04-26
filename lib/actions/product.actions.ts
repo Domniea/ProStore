24594d99ea37e8ebe66dbde04aa9ef91796a304b
domniea <domniea@gmail.com>
2025-04-26 15:05:32 -0600

    Admin Create and  Update Scheme and started form componant

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,11 @@
 'use server'
 
 import { prisma } from "@/db/prisma"
-import { convertToPlainObject } from "../utils"
+import { convertToPlainObject, formatError } from "../utils"
+import { PAGE_SIZE } from "../constants"
+import { revalidatePath } from "next/cache"
+import { insertProductSchema, updateProductSchema } from "../validators"
+import { z } from "zod"
 
 //Get latest products
 export async function getLatestProducts() {
@@ -21,4 +25,96 @@ export async function getProductBySlug(slug: string) {
         where: { slug: slug }
     })
 
+}
+
+// Get all products
+export async function GetAllProducts({
+    // query,
+    limit = PAGE_SIZE,
+    page,
+    // category
+}: {
+    query: string,
+    limit?: number,
+    page: number,
+    category: string
+}) {
+    const data = await prisma.product.findMany({
+        skip: (page -1) * limit,
+        take: limit
+    })
+
+    const dataCount = await prisma.product.count()
+
+    return {
+        data,
+        totalPages: Math.ceil(dataCount / limit)
+    }
+}
+
+// Delete a product
+export async function DeleteProduct(id: string) {
+    try {
+        const productExist = await prisma.product.findFirst({
+            where: { id }
+        })
+
+        if (!productExist) throw new Error('Product is not found')
+
+        await prisma.product.delete({
+            where: {id}
+        })
+        
+        revalidatePath('/admin/products')
+        return { 
+            success: true,
+            message: 'Product deleted successfully'
+         }
+    } catch (error) {
+        return { success: false, message: formatError(error) }
+    }
+}
+
+// Create product
+export async function createProduct(data: z.infer<typeof insertProductSchema>) {
+    try {
+        const product = insertProductSchema.parse(data)
+
+        await prisma.product.create({data: product})
+
+        revalidatePath(`/admin/products`)
+
+        return {
+            success: false,
+            message: 'Product created successfully'
+        }
+    } catch (error) {
+        return { success: false, message: formatError(error) }
+    }
+} 
+
+// Update product
+export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
+    try {
+        const product = updateProductSchema.parse(data)
+        const productExists = await prisma.product.findFirst({
+            where: { id: product.id}
+        })
+
+        if (!productExists) throw new Error('Product not found')
+
+        await prisma.product.update({
+            where: { id: data.id},
+            data: product
+        })
+
+        revalidatePath(`/admin/products`)
+
+        return {
+            success: false,
+            message: 'Product updated successfully'
+        }
+    } catch (error) {
+        return { success: false, message: formatError(error) }
+    }
 }
