5847fcfd2087966b2df972ab92563b65c1872f76
domniea <domniea@gmail.com>
2025-06-05 16:47:30 -0600

    webhook test1

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute='class'
-          defaultTheme='light'
+          // defaultTheme='light'
+          defaultTheme='system'
           enableSystem
           disableTransitionOnChange
 
