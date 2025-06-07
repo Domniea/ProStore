ecc56ab0c4ed25951d6099cc295ff90c37a77b9b
domniea <domniea@gmail.com>
2025-06-07 10:52:13 -0600

    email template set to send

--- lib/validators.ts
+++ lib/validators.ts
@@ -71,7 +71,7 @@ export const shippingAddressSchema = z.object({
     fullName: z.string().min(6, 'Name must be at least 3 characters'),
     streetAddress: z.string().min(6, 'Address must be at least 3 characters'),
     city: z.string().min(6, 'City must be at least 3 characters'),
-    postalCode: z.string().min(6, 'Postal code must be at least 6 characters'),
+    postalCode: z.string().min(5, 'Postal code must be at least 5 characters'),
     country: z.string().min(3, 'Name must be at least 3 characters'),
     lat: z.number().optional(),
     lng: z.number().optional()
